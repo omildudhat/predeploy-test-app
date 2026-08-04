11783dae6242ab4d1bfa306be8a4d4ce7071d66e
Ynova019 <ynova019@gmail.com>
2026-08-04 11:51:57 -0500

    test: trigger mixed Kubernetes and Terraform risks

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,23 +2,18 @@ resource "aws_security_group" "database" {
   name = "database-sg"
 
   ingress {
-    description = "Private PostgreSQL access"
+    description = "Public PostgreSQL access"
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/8"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_db_instance" "payments" {
   identifier          = "payments-db"
   engine              = "postgres"
-  publicly_accessible = false
+  publicly_accessible = true
   storage_encrypted   = true
-  deletion_protection = true
-}
-
-resource "aws_nat_gateway" "primary" {
-  allocation_id = "eipalloc-test"
-  subnet_id     = "subnet-test"
+  deletion_protection = false
 }
